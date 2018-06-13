469a447ca10fc921fc712694eea399e9710c0f8a
Danyllo <danyllof.gps@gmail.com>
2018-06-13 16:23:29 -0300

    User | install

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,14 @@
 <?php
+
+Route::get('/usercreate',function (){
+    $data = [
+        'name'      => 'apigateway',
+        'email'     => 'apivision@mangue3.com',
+        'api_token' => bcrypt('zinabrevisionapi'),
+        'password'  => bcrypt('zinabrevisionapi'),
+    ];
+    return \App\User::create($data);
+});
 Route::group(['middleware' => ['auth:api']], function () {
     
     Route::get('/cep/{cep}',    'CorreiosController@findCep');
